81cf9eb854c15d05c4db403c61a207a7ef635b63
Vitaliy Yurakh <109657047+VitaliyYurakh@users.noreply.github.com>
2024-07-23 16:48:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyBDzQITOC9RlEIFERSCRVvB_a6ztMMEBXw",
-    authDomain: "third-ui.firebaseapp.com",
-    databaseURL: "https://third-ui-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "third-ui",
-    storageBucket: "third-ui.appspot.com",
-    messagingSenderId: "924310054290",
-    appId: "1:924310054290:web:cf13d47209cb7550458d4b"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 });
 
 const auth = firebase.auth()
